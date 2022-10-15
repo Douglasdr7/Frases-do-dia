546805ec664f885ba46e7c4ac593f2171aea1a8e
Your Name Here <douglas.dr.ramos@gmail.com>
2022-10-15 12:19:46 -0300

    Creation of an app that give us frases of the day.

---
+++ app/src/main/java/br/com/doug/frasesdodia/MainActivity.java
@@ -0,0 +1,34 @@
+package br.com.doug.frasesdodia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void gerarNovaFrase(View view){
+        String[] frases={
+                "frase1",
+                "frase2",
+                "frase3",
+                "frase4",
+                "frase5",
+                "frase6"
+        };
+        int numero = new Random().nextInt(6);
+        TextView texto = findViewById(R.id.tv_frase);
+        texto.setText(frases[numero]);
+
+    }
+}
